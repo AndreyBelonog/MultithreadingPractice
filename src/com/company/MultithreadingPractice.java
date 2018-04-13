b89b42ff05ee5825adf9dc80b4e8c2e57d0fe65b
AndreyBelonog <36847003+AndreyBelonog@users.noreply.github.com>
2018-04-13 21:29:58 +0300

    Added bug into code to practice my VCS skills

--- src/com/company/MultithreadingPractice.java
+++ src/com/company/MultithreadingPractice.java
@@ -16,10 +16,19 @@ public class MultithreadingPractice {
                 mainCounter++;
             }
         });
+
+        Thread thread2 = new Thread(() -> {
+           for(int i = 0; i < 10000; i++){
+               mainCounter++;
+           }
+        });
+
         thread1.start();
+        thread2.start();
 
         try{
             thread1.join();
+            thread2.join();
         }catch (InterruptedException ex){
             ex.printStackTrace();
         }
